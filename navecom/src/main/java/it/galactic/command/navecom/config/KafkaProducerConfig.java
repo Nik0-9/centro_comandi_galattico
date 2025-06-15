60357f84c0a37d0ccaf2e2cf65ad708aded7f18d
nicola-filannino <filannino.nicola9@gmail.com>
2025-06-15 18:09:51 +0200

    fix(KafkaProducerConfig): changed constructor parameter ProducerFactory<String, Object> to <String, GalaxyMissionDTO>

--- navecom/src/main/java/it/galactic/command/navecom/config/KafkaProducerConfig.java
+++ navecom/src/main/java/it/galactic/command/navecom/config/KafkaProducerConfig.java
@@ -9,6 +9,9 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import it.galactic.command.navecom.dto.GalaxyMissionDTO;
+
 import java.util.HashMap;
 import java.util.Map;
 /*
@@ -22,18 +25,19 @@ public class KafkaProducerConfig {
      private String bootstrapServers;
  
      @Bean
-     public ProducerFactory<String, Object> producerFactory() {
-         Map<String, Object> configProps = new HashMap<>();
-         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-         // Configuriamo il Value Serializer per usare JSON
-         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-         return new DefaultKafkaProducerFactory<>(configProps);
-     }
- 
-     @Bean
-     public KafkaTemplate<String, Object> kafkaTemplate() {
-         return new KafkaTemplate<>(producerFactory());
-     }
-    
+     public ProducerFactory<String, GalaxyMissionDTO> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        
+        // MODIFICATO: La factory ora è tipizzata correttamente
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    // MODIFICATO: Il KafkaTemplate ora ha il tipo esatto richiesto dal service
+    public KafkaTemplate<String, GalaxyMissionDTO> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
