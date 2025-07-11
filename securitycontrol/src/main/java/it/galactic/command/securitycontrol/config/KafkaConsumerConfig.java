2e1e4bd38eaca688ae70c5b1dfc35a4d71ff973f
nicola.filannino <nicola.filannino9@gmail.com>
2025-07-11 19:03:54 +0200

    feat(KafkaConsumerConfig.java) configurationt not work

---
+++ securitycontrol/src/main/java/it/galactic/command/securitycontrol/config/KafkaConsumerConfig.java
@@ -0,0 +1,52 @@
+package it.galactic.command.securitycontrol.config;
+
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka // Abilita il supporto ai listener Kafka di Spring
+@Configuration
+public class KafkaConsumerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        // Dice al deserializer di fidarsi di tutti i pacchetti.
+        // Per maggiore sicurezza, potresti specificare il pacchetto esatto: "it.galactic.command.securitycontrol.dto"
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
+        // Specifica il tipo di oggetto di default che ci si aspetta di deserializzare.
+        // Anche se non sempre necessario, aiuta a evitare ambiguita.
+        // props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "it.galactic.command.securitycontrol.dto.GalaxyMissionDTO");
+
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
